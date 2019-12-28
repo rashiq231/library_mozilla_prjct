4d93f406bda0ddbfe9f98a2462b5db76937fbd2e
rashiq sheik <mail2.rashiq@gmmail.com>
2019-12-28 13:16:34 +0530

    completion of models lecture

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -35,6 +35,12 @@ class Book(models.Model):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
 
+    def display_genre(self):
+        """Creating a field for Genre this is required to display genre in Admin"""
+        return ",".join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
 import uuid # Required for unique book instances
 
 class BookInstance(models.Model):
